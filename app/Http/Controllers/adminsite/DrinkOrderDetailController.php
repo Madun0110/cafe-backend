36873c63b5f46d0995e75767e1c129e8abc3cfef
Madun0110 <fikriocta15@gmail.com>
2025-11-04 13:44:46 +0700

    Change data retrieval to use Orders

--- app/Http/Controllers/adminsite/DrinkOrderDetailController.php
+++ app/Http/Controllers/adminsite/DrinkOrderDetailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\adminsite;
 
+use App\Models\Orders;
 use Illuminate\Http\Request;
 use App\Models\DrinkOrderDetails;
 use App\Http\Controllers\Controller;
@@ -10,7 +11,7 @@ class DrinkOrderDetailController extends Controller
 {
     public function getAllDrinkOrderDetails()
     {
-        $data = DrinkOrderDetails::with(['order', 'product'])->get();
+        $data = Orders::with(['drink.product'])->get();
         return response()->json([
             'status' => true,
             'message' => 'success',
