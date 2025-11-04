bcbb10268386810c1d82797dbcddcaf4e8ccda87
Madun0110 <fikriocta15@gmail.com>
2025-11-04 13:45:00 +0700

    Update FoodOrderDetailController.php

--- app/Http/Controllers/adminsite/FoodOrderDetailController.php
+++ app/Http/Controllers/adminsite/FoodOrderDetailController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\adminsite;
 use Illuminate\Http\Request;
 use App\Models\FoodOrderDetails;
 use App\Http\Controllers\Controller;
+use App\Models\Orders;
 
 class FoodOrderDetailController extends Controller
 {
     public function getAllFoodOrderDetails()
     {
-        $data = FoodOrderDetails::with(['order', 'product'])->get();
-        return $data;
+        $data = Orders::with(['food'])->get();
+        // return $data;
         return response()->json([
             'status' => true,
             'message' => 'success',
